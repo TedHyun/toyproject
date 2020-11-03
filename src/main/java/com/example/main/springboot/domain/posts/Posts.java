f3dc8bc3376c4743e261cc1fd16b391956f311ba
hyundong.lee <hyundong.lee@sein-it.com>
2020-11-03 16:22:43 +0900

    201103 change

--- src/main/java/com/example/main/springboot/domain/posts/Posts.java
+++ src/main/java/com/example/main/springboot/domain/posts/Posts.java
@@ -13,7 +13,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,4 +34,8 @@ public class Posts {
         this.author = author;
     }
 
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
---
+++ src/main/java/com/example/main/springboot/service/posts/PostsService.java
@@ -0,0 +1,38 @@
+package com.example.main.springboot.service.posts;
+
+import com.example.main.springboot.domain.posts.Posts;
+import com.example.main.springboot.domain.posts.PostsRepository;
+import com.example.main.springboot.web.dto.PostsResponseDto;
+import com.example.main.springboot.web.dto.PostsSaveRequestDto;
+import com.example.main.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestsDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id =" + id));
+
+        posts.update(requestsDto.getTitle(), requestsDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("햬당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
+
