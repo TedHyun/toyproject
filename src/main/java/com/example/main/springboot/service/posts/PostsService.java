973b041edf7ab8a0a0d7dcaaa5fa6529bf88417e
whatupTed <gusehd525@gmail.com>
2020-11-05 15:17:43 +0900

    201105 change

--- src/main/java/com/example/main/springboot/service/posts/PostsService.java
+++ src/main/java/com/example/main/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.example.main.springboot.service.posts;
 
 import com.example.main.springboot.domain.posts.Posts;
 import com.example.main.springboot.domain.posts.PostsRepository;
+import com.example.main.springboot.web.dto.PostsListResponseDto;
 import com.example.main.springboot.web.dto.PostsResponseDto;
 import com.example.main.springboot.web.dto.PostsSaveRequestDto;
 import com.example.main.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -27,6 +31,13 @@ public class PostsService {
         posts.update(requestsDto.getTitle(), requestsDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDESC().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public PostsResponseDto findById (Long id){
